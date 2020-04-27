6886b44d6efb49cb9fbaaf4058662da83b5ccc6c
Joseph Strout <jstrout@lincoln.cs.colostate.edu>
2020-04-27 12:15:26 -0600

    Final commit of kmeans-complete code and data.

--- kmeans-complete/src/main/scala/kmeans.scala
+++ kmeans-complete/src/main/scala/kmeans.scala
@@ -3,6 +3,7 @@ package org.apache.spark.examples.mllib
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.mllib.clustering.{KMeans, KMeansModel}
 import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.ml.evaluation.ClusteringEvaluator
 
 import org.apache.spark.sql.SparkSession
 import scala.util.control.Breaks._
@@ -11,27 +12,38 @@ object kmeans {
   def main(args: Array[String]) {
 	println("Direct k-means clustering in Scala/Spark");
 
-	val conf = new SparkConf().setAppName("KMeans")
-	val sc = new SparkContext(conf)
+	val spark = SparkSession.builder.appName("kmeans").getOrCreate()
 
 	// Load and parse the data
-	val data = sc.textFile("sample_data.txt")
-	val parsedData = data.map(s => Vectors.dense(s.split(' ').map(_.toDouble))).cache()
+	val path = "/project/ch1-7-12-scaled.csv"
+	println("Reading " + path + "...")
+	val data = spark.read.textFile(path).rdd
+	val parsedData = data.map(s => Vectors.dense(s.split(',').map(_.toDouble))).cache()
 
 	// Cluster the data into two classes using KMeans
-	val numClusters = 2
+	val numClusters = 4
 	val numIterations = 20
 	val clusters = KMeans.train(parsedData, numClusters, numIterations)
 
 	// Evaluate clustering by computing Within Set Sum of Squared Errors
 	val WSSSE = clusters.computeCost(parsedData)
-	println(s"Within Set Sum of Squared Errors = $WSSSE")
+	println(s"Within Set Sum of Squared Errors (k=$numClusters)= $WSSSE")
 
-	// Save and load model
-//	clusters.save(sc, "target/org/apache/spark/KMeansExample/KMeansModel")
+//	val predictions = clusters.predict(parsedData);
+//	val evaluator = new ClusteringEvaluator()
+//	val silhouette = evaluator.evaluate(predictions)
+//	println(s"Silhouette with squared euclidean distance = $silhouette")
+
+	for (i <- 0 to 3) {
+		println(s"Cluster $i center: " + clusters.clusterCenters(i));
+	}
+
+	// Save model
+//	println("Writing model to disk")
+//	clusters.save(spark.sparkContext, "/project/KMeansModel")
 //	val sameModel = KMeansModel.load(sc, "target/org/apache/spark/KMeansExample/KMeansModel")
 
-	sc.stop()
+	spark.stop()
 
 
 	println("Program complete.");
